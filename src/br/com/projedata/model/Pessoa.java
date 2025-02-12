c578579b3f61ab1fd7374a44d3de715a50998d8d
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2025-02-12 11:54:31 -0300

    criação da entidade Pessoa

---
+++ src/br/com/projedata/model/Pessoa.java
@@ -0,0 +1,25 @@
+package br.com.projedata.model;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
