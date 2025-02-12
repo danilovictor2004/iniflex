6c3737320cbf4f5d62311208349399e4919bfbd3
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2025-02-12 12:54:27 -0300

    resolução do exercicio 3.3

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,8 @@
 import br.com.projedata.model.Funcionario;
 
 import java.sql.SQLOutput;
+import java.text.DecimalFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,6 +26,26 @@ public class Principal {
         System.out.println();
         for (Funcionario funcionario : funcionariosOrdenados) {
             System.out.println(funcionario.getNome());
+        }
+
+        // Imprimir todos os funcionários com todas suas informações, sendo que:
+        // • informação de data deve ser exibido no formato dd/mm/aaaa;
+        // • informação de valor numérico deve ser exibida no formatado com separador de milhar como ponto
+        // e decimal como vírgula.
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DecimalFormat decimalFormatter = new DecimalFormat("#,###.00");
+
+        for (Funcionario funcionario : funcionarios) {
+            String dataFormatada = funcionario.getDataNascimento().format(dateFormatter);
+            String salarioFormatado = decimalFormatter.format(funcionario.getSalario());
+
+            System.out.printf("""
+                       Nome: %s
+                       Data de Nascimento: %s
+                       Salário: %s
+                       Função: %s
+                    """, funcionario.getNome(), dataFormatada, salarioFormatado, funcionario.getFuncao());
+            System.out.println();
         }
 
     }
